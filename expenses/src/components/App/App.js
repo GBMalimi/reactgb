732591cbd213082f0c3c8f638b88ecd2ebdf2413
Goce Biskovski <GBMalimi>
2019-05-10 00:31:52 +0200

    all commponents

--- expenses/src/components/App/App.js
+++ expenses/src/components/App/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import {Route} from "react-router-dom";
 import {Login} from "../Login/Login";
 import {Register} from "../Register/Register";
+import {NewProduct} from "../NewProduct/NewProduct";
+import {Products} from "../Products/Products";
+import {Expenses} from "../Expenses/Expenses";
 
 export class App extends React.Component {
     render() {
@@ -9,6 +12,9 @@ export class App extends React.Component {
             < main id="app" >
                 <Route exact path="/" component={Login}/>
                 <Route path="/register" component={Register}/>
+                <Route path="/newproduct" component={NewProduct}/>
+                <Route path="/products" component={Products}/>
+                <Route path="/Expenses" component={Expenses}/>
             </main >
         )
     }
--- expenses/src/components/Register/Register.js
+++ expenses/src/components/Register/Register.js
@@ -9,31 +9,23 @@ export class Register extends React.Component {
                     <form className="header_register">
                         <label className="input_label">First Name</label>
                         <input className="input_fields" type="text" />
-
                         <label className="input_label">Last Name</label>
                         <input className="input_fields" type="text" />
-
                         <label className="input_label">E-mail</label>
                         <input className="input_fields" type="email" />
-
                         <label className="input_label">Date of Birth</label>
                         <input className="input_fields" type="date" />
-
                         <label className="input_label">Telephone</label>
                         <input className="input_fields" type="number" />
-
                         <label className="input_label">Country</label>
                         <input className="input_fields" type="text" />
-
                         <label className="input_label">Password</label>
                         <input className="input_fields" type="password" />
-
                         <input className="submit" type="submit" value="REGISTER" />
-                        <p className="p_signin">Or if you already have an account, <a className="link" href="./Login.html">Sing in</a></p>
+                        <p className="p_signin">Or if you already have an account,
+                            <a className="link" href="./Login.html">Sing in</a>
+                        </p>
                     </form>
-
-
-
                 </div>
             </section>
         )
