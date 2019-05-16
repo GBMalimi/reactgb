01044c3fc264bd47f94a2072eeca797e9593ef7c
GBMalimi <petar.pejoski32@gmail.com>
2019-05-16 21:59:38 +0200

    fdsf

--- expense-report/index.js
+++ expense-report/index.js
@@ -1,56 +1,114 @@
-const users = require(`./models/users`);
 const products = require(`./models/products`);
 const express = require(`express`);
 var app = express();
+const dba=require("./connection");
+const User=require("./models/users")
 const myParser = require("body-parser");
 const session = require(`express-session`);
 app.use(session({ secret: "test" }));
 
+app.listen(8005)
 
+app.get ("/", (req,res)=>{
+    res.send("hello");
+})
 
 app.use(myParser.urlencoded({ extended: true }));
 
-var u1 = new users.create("admin", "admin", "admin@yahoo.com", "00000", "birthDate", "telephone", "country")
 
 
-app.post(`/register`, (req, res) => {
+app.post(`/register`, (req, res, next) => {
     var firstname = req.body.firstname;
     var lastname = req.body.lastname;
     var email = req.body.email;
-    var pass = req.body.pass;
+    var password = req.body.pass;
     var birthDate = req.body.birthDate;
     var telephone = req.body.telephone;
     var country = req.body.country;
 
-    var newUser = new users.create(email, pass, firstname, lastname, birthDate, telephone, country);
+    let user=new User({
+        firstName: firstname,
+        lastName: lastname,
+        email: email,
+        password: password,
+        birthDate: birthDate,
+        telephone: telephone,
+        country: country
+                
+    })
+
+    user.save(function(err){
+        if(err){
+            return next(err);
+        }
+        else{
+            res.send("User saved")
+        }
+    })
 });
 
-app.post(`./login`, (req, res) => {
-    var email = req.body.emailLogin;
-    var password = req.body.passwordLogin;
+app.post(`/newproduct`, (req,res, next)=>{
+    var productName=req.body.productname;
+    var productDescription=req.body.productdescription;
+    var productType=req.body.producttype;
+    var prooductDate=req.body.prooductdate;
+    var productPrice=req.body.productprice;
 
-    // database checks
+    let product=new Product({
+        productName: productname,
+        productDescription: productdescription,
+        productType: producttype,
+        prooductDate: prooductdate,
+        productPrice: productprice
+    })
 
-    req.session.user = email;
-    // return response to Frontend
+    user.save(function(err){
+        if(err){
+            return next(err);
+        }
+        else{
+            res.send("User saved")
+        }
+    })
 })
 
-app.post(`./addProduct`, (req,res)=>{
-    if(req.session.email){
-        var productName=req.body.productName;
-        var productDescription=re.body.productDescription;
-        var productType=req.body.productType;
-        var purchaseDate=req.body.purchaseDate;
-        var price=req.body.price;
-        var userEmail=req.session.email;
+// app.post(`./login`, (req, res) => {
+//     var email = req.body.emailLogin;
+//     var password = req.body.passwordLogin;
 
-        var p = new products.create(productName,productDescription,productType,purchaseDate,price,userEmail);
-        // send response to FE
-    }
-    else {
-        res.status(403).send("Access denied");
-    }
+//     // database checks
 
-})
+//     req.session.user = email;
+//     // return response to Frontend
+// })
+
+// app.post(`./addProduct`, (req,res)=>{
+//     if(req.session.email){
+//         var productName=req.body.productName;
+//         var productDescription=re.body.productDescription;
+//         var productType=req.body.productType;
+//         var purchaseDate=req.body.purchaseDate;
+//         var price=req.body.price;
+//         var userEmail=req.session.email;
+
+//         var p = new products.create(productName,productDescription,productType,purchaseDate,price,userEmail);
+//         // send response to FE
+//     }
+//     else {
+//         res.status(403).send("Access denied");
+//     }
+
+// })
 
 // console.log(ul);
+
+
+
+app.get("/products", (req, res, next)=>{
+    products.find({},function(err, products){
+        if(err) {
+            return next(err);
+        }
+        res.send(products);
+    })
+})
--- expense-report/models/users.js
+++ expense-report/models/users.js
@@ -1,9 +1,29 @@
-exports.create = function (fn,ln,email,pass,birthDay,telephone,country) {
-    this.firstname=fn;
-    this.lastname=ln;
-    this.email=email;
-    this.pass=pass;
-    this.birthDate=birthDay;
-    this.telephone=telephone;
-    this.country=country;
-}
+// exports.create = function (fn,ln,email,pass,birthDay,telephone,country) {
+//     this.firstname=fn;
+//     this.lastname=ln;
+//     this.email=email;
+//     this.pass=pass;
+//     this.birthDate=birthDay;
+//     this.telephone=telephone;
+//     this.country=country;
+// }
+
+
+const mongoose=require(`mongoose`);
+const Schema=mongoose.Schema;
+
+var usersSchema=new Schema({
+    firstName:{type: String},
+    lastName: {type: String},
+    email: {type: String},
+    password: {type: String},
+    birthDate: {type: String},
+    telephone: {type: String},
+    country: {type: String},
+    
+    
+});
+
+var User=mongoose.model(`User`, usersSchema);
+
+module.exports=User;
